7bcbf11f0e48b64edf83b7068a21bc111515e534
madan-wego <madan@wego.com>
2024-01-09 22:59:52 +0530

    k6 load test

---
+++ k6/east1.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import {sleep} from 'k6';
+
+export const options = {
+  // Key configurations for avg load test in this section
+  stages: [
+    { duration: '1m', target: 100 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
+  ],
+};
+
+export default () => {
+  const urlRes = http.get('https://east1-latency.wego.com/todos');
+  sleep(1);
+};
+
