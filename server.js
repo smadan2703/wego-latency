787e4ad253ecf7f16bd40e7c601a6fed8053a548
madan-wego <madan@wego.com>
2024-03-06 00:30:32 +0530

    ssl config

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const noCache = require('nocache');
 const routes = require("./routes");
-
 
 // App
 const app = express();
 app.use(noCache());
 app.set('etag', false);
+
 // Set port
-const port = process.env.PORT || "80";
+const port = process.env.PORT || 443; // Default HTTPS port
 app.set("port", port);
 
 app.use('/', routes);
 
-// Server
-app.listen(port, () => console.log(`Server running on localhost:${port}`));
+// HTTPS options
+const options = {
+  key: fs.readFileSync("/root/wego-latency/private.pem"),
+  cert: fs.readFileSync("/root/wego-latency/cert.pem")
+};
+
+// Create HTTPS server
+const server = https.createServer(options, app);
+
+// Start server
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
